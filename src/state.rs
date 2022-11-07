15eff9a59f8ac6b3f567747b3ae665a9e3afe045
EG-easy <1018eguchi@gmail.com>
2022-11-07 09:34:47 +0900

    update state

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,8 @@
-use cosmwasm_std::Addr;
+use cosmwasm_std::{Addr, Timestamp};
 use cw_storage_plus::Map;
 
 pub const CONTROLLERS: Map<&Addr, Addr> = Map::new("controller");
 pub const CHANGED: Map<&Addr, u64> = Map::new("changed");
 pub const NONCE: Map<&Addr, u64> = Map::new("nonce");
+pub const ATTRIBUTES: Map<(&Addr, &String), Vec<String>> = Map::new("attribute");
+pub const VALIDITIES: Map<(&Addr, &String, &String), Timestamp> = Map::new("validities");
